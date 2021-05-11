191bd5ee0e47ff0a4cbf1173c527b6197970b3f9
acapodilupo <anthony.capodilupo@techdata.com>
2021-05-11 13:02:14 -0400

    Adding MGT server and RSAT script

---
+++ DomainController/Scripts/getRSAT.ps1
@@ -0,0 +1,13 @@
+﻿
+#Install Chocolatey
+try{
+    
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+}
+catch{
+    
+     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+
+}
+
+choco install rsat -yes --ignore-checksums
