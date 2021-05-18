5956f2071a6b8389dc7519ddd914a7a9dbec9541
acapodilupo <anthony.capodilupo@techdata.com>
2021-05-18 08:30:42 -0400

    Added ADC script to secondary server

---
+++ DomainController/DSC/dsc_configureserver.ps1
@@ -0,0 +1,51 @@
+﻿configuration configureServer 
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [Bool]$installIIS,
+        [Bool]$installDHCP
+
+
+        
+    ) 
+    
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Node localhost
+    {
+
+    ##configures IIS if true
+    if($installIIS){
+
+        WindowsFeature WebServer
+        {
+          Name = "Web-Server"
+          Ensure = "Present"
+        }
+
+        WindowsFeature ManagementTools
+        {
+          Name = "Web-Mgmt-Tools"
+          Ensure = "Present"
+        }
+
+        WindowsFeature DefaultDoc
+        {
+          Name = "Web-Default-Doc"
+          Ensure = "Present"
+        }
+    }
+
+    if($installDHCP){
+        WindowsFeature DHCPServer
+        {
+          Name = "DHCP"
+          Ensure = "Present"
+        }
+
+      }
+
+
+
+   }
+} 
