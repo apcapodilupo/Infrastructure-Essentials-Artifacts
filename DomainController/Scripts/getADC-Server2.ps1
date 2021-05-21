72d3d403c52ce188c89e4f5b647af08a7b52d9d6
acapodilupo <anthony.capodilupo@techdata.com>
2021-05-21 12:32:24 -0400

    added ADC script for second server

---
+++ DomainController/Scripts/getADC-Server2.ps1
@@ -0,0 +1,5 @@
+﻿
+    #install chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+    choco install azure-ad-connect -yes --ignore-checksums
+
