9c05854157522d3be1710489b973a9c8d7d77bcc
acapodilupo <anthony.capodilupo@techdata.com>
2021-05-21 14:03:08 -0400

    Update getADC.ps1

--- DomainController/Scripts/getADC.ps1
+++ DomainController/Scripts/getADC.ps1
@@ -1,6 +1,31 @@
 ﻿
-    #install chocolatey
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
-    choco install azure-ad-connect -yes --ignore-checksums
-    #Create the Storage Accounts OU
-    New-ADOrganizationalUnit -Name "Storage Accounts"
+    try{ 
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+
+    }
+    catch{
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+
+    }
+
+
+     try{ 
+            choco install azure-ad-connect -yes --ignore-checksums
+
+    }
+    catch{
+            choco install azure-ad-connect -yes --ignore-checksums
+
+    }
+
+
+     try{ 
+        New-ADOrganizationalUnit -Name "Storage Accounts"
+
+    }
+    catch{
+        New-ADOrganizationalUnit -Name "Storage Accounts"
+
+    }
+
+    exit 0
